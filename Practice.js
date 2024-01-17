701b164a9d30154bca1991ce3fc57cbd3aa4b5a3
Suraj Dubey <139530741+SurajDubey14@users.noreply.github.com>
2024-01-17 23:11:54 +0530

    Add files via upload

---
+++ Practice.js
@@ -0,0 +1,31 @@
+// let a = '2';
+// let b = 2;
+
+// let str = "Suraj Pandat";
+// let arr = [1,2,3,4,5];
+
+// const employee = {
+//     fullName : "Golu Dubey" ,
+//     age : 52,
+//     weight : 68
+// }
+// // if(a===b){
+// //     console.log("Suraj");
+// // }else{
+// //     console.log("Error");
+// // }
+
+// for(let i of arr){
+//     console.log("i = "+ i);
+// }
+
+function hello(){
+    console.log("Syed yatichu");
+}
+
+
+console.log("Hello");
+console.log("Hello");
+setTimeout(hello,5000);
+console.log("Hello");
+console.log("Hello");
